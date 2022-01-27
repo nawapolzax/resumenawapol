4a6414f7dea6aa1cac9e6a2275e0700610528d2d
nawapolzax <98540478+nawapolzax@users.noreply.github.com>
2022-01-27 23:04:39 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$host="locallhost";
+$username="root";
+$password="12345678";
+$database="database";
+$conn=mysqli_connect($host,$username,$password,$database);
+mysqli_set_charset($conn,"utf8");
+?>
